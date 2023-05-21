e25863ec166b105a0c5622db3b33a3ff9fb78a09
FernandoGaribay <garibaycejaf@gmail.com>
2023-05-20 18:33:04 -0600

    Factorizacion conexion.java

--- src/main/conexion.java
+++ src/main/conexion.java
@@ -3,40 +3,34 @@ package main;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class conexion {
 
-    String bd = "motorinferencia";
-    String url = "jdbc:mysql://localhost:3308/";
+    private String bd = "motorinferencia";
+    private String url = "jdbc:mysql://localhost:3308/";
 
-    String user = "prueba";
-    String password = "";
-    String driver = "com.mysql.cj.jdbc.Driver";
-    Connection connect;
+    private String user = "prueba";
+    private String password = "";
+    private String driver = "com.mysql.cj.jdbc.Driver";
+    private Connection connect;
 
     public conexion() {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName(driver);
         } catch (ClassNotFoundException ex) {
             System.out.println("Error al cargar el driver");
             System.out.println(ex.getMessage());
-
         }
     }
 
-    public Connection conectar() {
+    public void conectar() {
         try {
-            Class.forName(driver);
             connect = DriverManager.getConnection(url + bd + "?serverTimezone=GMT-5", user, password);
             System.out.println("Se logró la conexion");
-
-        } catch (ClassNotFoundException | SQLException ex) {
-            Logger.getLogger(conexion.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
             System.out.println("No se conecto a bd");
+            System.out.println(ex.getMessage());
         }
-        return connect;
     }
 
     public void desconectar() {
@@ -44,13 +38,14 @@ public class conexion {
             connect.close();
             System.out.println("Se logró desconectar");
         } catch (SQLException ex) {
-            Logger.getLogger(conexion.class.getName()).log(Level.SEVERE, null, ex);
             System.out.println("no se desconecto");
+            System.out.println(ex.getMessage());
         }
     }
 
     public static void main(String[] args) {
         conexion conect = new conexion();
         conect.conectar();
+        conect.desconectar();
     }
 }
